266d815465185e9f07563b65ad5647da028b89aa
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-02-23 22:49:26 +0530

    some changes

--- DB/database.js
+++ DB/database.js
@@ -17,8 +17,6 @@ cloudinary.config({
 const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         console.log(`✅ Database Connected: ${connect.connection.host}`);
     } catch (error) {
